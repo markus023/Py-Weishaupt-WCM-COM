eff827b33fffa899ad8d768406e64459db9e5911
Pieter Hamels <pieter@hamels.be>
2022-08-23 10:38:11 +0200

    Added sensors

--- weishaupt_wcm_com/heat_exchanger.py
+++ weishaupt_wcm_com/heat_exchanger.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPDigestAuth
 
 ENDPOINT = "/parameter.json"
 QUERYTELEGRAM = (
-    '{"prot":"coco","telegramm":[[10,0,1,4176,0,0,0,0],[10,0,1,3793,0,0,0,0],[10,0,1,3792,0,0,0,0],[10,0,1,12,0,0,0,0],[10,0,1,14,0,0,0,0],[10,0,1,3101,0,0,0,0],[10,0,1,325,0,0,0,0],[10,0,1,3197,0,0,0,0],[6,0,1,5,0,0],[6,0,1,274,0,0],[6,0,1,8,0,0]]}'
+    '{"prot":"coco","telegramm":[[10,0,1,4176,0,0,0,0],[10,0,1,3793,0,0,0,0],[10,0,1,3792,0,0,0,0],[10,0,1,12,0,0,0,0],[10,0,1,14,0,0,0,0],[10,0,1,3101,0,0,0,0],[10,0,1,325,0,0,0,0],[10,0,1,3197,0,0,0,0],[6,0,1,5,0,0],[6,0,1,274,0,0],[6,0,1,8,0,0],[10,0,1,81,0,0,0,0],[10,0,1,1497,0,0,0,0],[10,0,1,1498,0,0,0,0], [10,0,1,466,0,0,0,0], [10,0,1,82,0,0,0,0], [10,0,1,83,0,0,0,0], [10,0,1,1,0,0,0,0], [10,0,1,373,0,0,0,0]]}'
 )
 
 # [10,0,1,4176,0,0,0,0],
@@ -15,7 +15,7 @@ TEMP = 2
 DECIMAL_VALUE = 3
 
 # ID, Name, Value/Temp
-QUERIES = [[3793, "Oil Meter", VALUE], [4176, "Load Setting", DECIMAL_VALUE], [12, "Outside Temperature", TEMP], [14, "Warm Water Temperature", TEMP], [3101, "Flow Temperature", TEMP], [325, "Flue Gas Temperature", TEMP], [5, "Room Temperature", TEMP], [274, "Operation Mode", VALUE], [8, "Mixed External Temperature"]]
+QUERIES = [[3793, "Oil Meter", VALUE], [4176, "Load Setting", DECIMAL_VALUE], [12, "Outside Temperature", TEMP], [14, "Warm Water Temperature", TEMP], [3101, "Flow Temperature", TEMP], [325, "Flue Gas Temperature", TEMP], [5, "Room Temperature", TEMP], [274, "Operation Mode", VALUE], [8, "Mixed External Temperature"], [81, "Flame", VALUE], [1497, "Gas Valve 1", VALUE], [1498, "Gas Valve 2", VALUE], [466, "Pump", VALUE], [82, "Heating", VALUE], [83, "Warm Water", VALUE], [1, "Error", VALUE], [373, "Operation Phase", VALUE]]
 
 
 def getTemperture(lowByte, highByte):
